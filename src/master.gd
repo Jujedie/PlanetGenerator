ae9fb6d32a79f04e8d6040b0f722c18d6fc0bba7
Jujedie <=>
2025-04-27 22:55:02 +0200

    Mise à jour des curseurs de la scène principale : ajustement des valeurs minimales et maximales, ajout de nouvelles connexions pour les boutons de confirmation, sauvegarde, suivant et précédent.

--- src/master.gd
+++ src/master.gd
@@ -1,20 +1,25 @@
 extends Node2D
 
+var planetGenerator : PlanetGenerator
+var map_actuelle    : Image
+var maps            : Array[Image]
+var map_index       : int = 0
+
 func _ready() -> void:
 	var label = $Node2D/Control/sldRayonPlanetaire/Node2D/Label
-	label.text = "Rayon Planétaire : 0 | 0 <-> 1"
+	label.text = "Rayon Planétaire : 2000"
 	label = $Node2D/Control/sldTempMoy/Node2D/Label
 	label.text = "Température Moyenne : -273"
 	label = $Node2D/Control/sldHautEau/Node2D/Label
 	label.text = "Elevation des mers : -100"
 	label = $Node2D/Control/sldPrecipitationMoy/Node2D/Label
-	label.text = "Précipitation Moyenne : 0 | 0 <-> 1"
+	label.text = "Précipitation Moyenne : 0.00 | 0 <-> 1"
 	pass
 
 func _on_sld_rayon_planetaire_value_changed(value: float) -> void:
 	var sld = $Node2D/Control/sldRayonPlanetaire
 	var label = $Node2D/Control/sldRayonPlanetaire/Node2D/Label
-	label.text = "Rayon Planétaire : "+str(sld.value)+" | 0 <-> 1"
+	label.text = "Rayon Planétaire : "+str(sld.value)
 
 
 func _on_sld_temp_moy_value_changed(value: float) -> void:
@@ -32,4 +37,42 @@ func _on_sld_haut_eau_value_changed(value: float) -> void:
 func _on_sld_precipitation_moy_value_changed(value: float) -> void:
 	var sld = $Node2D/Control/sldPrecipitationMoy
 	var label = $Node2D/Control/sldPrecipitationMoy/Node2D/Label
-	label.text = "Précipitation Moyenne : "+str(sld.value)+" | 0 <-> 1"
+	var value_str = str(sld.value)
+	if len(value_str) != 4:
+		value_str = value_str + "0"
+	label.text = "Précipitation Moyenne : "+value_str+" | 0 <-> 1"
+
+
+func _on_btn_comfirme_pressed() -> void:
+	var nom = $Node2D/Control/planeteName/LineEdit
+	print("Nom de la planète : "+nom.text)
+	var sldRayonPlanetaire = $Node2D/Control/sldRayonPlanetaire
+	print("Rayon Planétaire : "+str(sldRayonPlanetaire.value))
+	var sldTempMoy = $Node2D/Control/sldTempMoy
+	print("Température Moyenne : "+str(sldTempMoy.value))
+	var sldHautEau = $Node2D/Control/sldHautEau
+	print("Elevation des mers : "+str(sldHautEau.value))
+	var sldPrecipitationMoy = $Node2D/Control/sldPrecipitationMoy
+	print("Précipitation Moyenne : "+str(sldPrecipitationMoy.value)+"\n")
+
+	planetGenerator = PlanetGenerator.new(nom.text, sldRayonPlanetaire.value, sldTempMoy.value, sldHautEau.value, sldPrecipitationMoy.value )
+	
+	print("Génération de la planète : "+nom.text)
+	planetGenerator.generate_planet()
+	maps = planetGenerator.get_maps()
+
+func _on_btn_sauvegarder_pressed() -> void:
+	planetGenerator.save_planet()
+
+
+func _on_btn_suivant_pressed() -> void:
+	map_index += 1
+	if map_index >= maps.size():
+		map_index = 0
+	$Node2D/Node2D/Control/SubViewportContainer/SubViewport/Fond/Map.texture = maps[map_index]
+
+func _on_btn_precedant_pressed() -> void:
+	map_index -= 1
+	if map_index < 0:
+		map_index = maps.size() - 1
+	$Node2D/Node2D/Control/SubViewportContainer/SubViewport/Fond/Map.texture = maps[map_index]
