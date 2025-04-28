e2ee7b52903906514c668c3cebb84345c68be694
Jujedie <=>
2025-04-28 23:35:38 +0200

    Mise à jour des curseurs et des étiquettes dans l'interface utilisateur, ajustement des valeurs par défaut et amélioration de la logique de génération des cartes.

--- src/master.gd
+++ src/master.gd
@@ -5,19 +5,24 @@ var map_actuelle    : Image
 var maps            : Array[Image]
 var map_index       : int = 0
 
+# TO DO : SAUVEGARDER AUTOMATIQUEMENT LES IMAGES DANS UN DOSSIER PREDEFINI TEMPORAIRE PUIS LES RECHARGER VIA LOAD
+
 func _ready() -> void:
+	var sldRayonPlanetaire = $Node2D/Control/sldRayonPlanetaire
 	var label = $Node2D/Control/sldRayonPlanetaire/Node2D/Label
-	label.text = "Rayon Planétaire : 100"
-
-	label = $Node2D/Control/sldTempMoy/Node2D/Label
-	label.text = "Température Moyenne : 18"
-
+	label.text = "Rayon Planétaire : "+str(sldRayonPlanetaire.value)
+	
+	var sldTempMoy = $Node2D/Control/sldTempMoy
 	label = $Node2D/Control/sldHautEau/Node2D/Label
-	label.text = "Elevation des mers : 0"
-
+	label.text = "Température Moyenne : "+str(sldTempMoy.value)
+	
+	var sldHautEau = $Node2D/Control/sldHautEau
+	label = $Node2D/Control/sldHautEau/Node2D/Label
+	label.text = "Elevation des mers : "+str(sldHautEau.value)
+	
+	var sldPrecipitationMoy = $Node2D/Control/sldPrecipitationMoy
 	label = $Node2D/Control/sldPrecipitationMoy/Node2D/Label
-	label.text = "Précipitation Moyenne : 0.50 | 0 <-> 1"
-	pass
+	label.text = "Précipitation Moyenne : "+str(sldPrecipitationMoy.value)+"\n"
 
 func _on_sld_rayon_planetaire_value_changed(value: float) -> void:
 	var sld = $Node2D/Control/sldRayonPlanetaire
@@ -64,7 +69,13 @@ func _on_btn_comfirme_pressed() -> void:
 	planetGenerator.generate_planet()
 	maps = planetGenerator.getMaps()
 
-	$Node2D/Node2D/Control/SubViewportContainer/SubViewport/Fond/Map.texture = ImageTexture.create_from_image(maps[map_index])
+	print(maps[map_index])
+	
+	var itex = ImageTexture.new()
+	itex.create_from_image(maps[map_index])
+	print(itex)
+
+	$Node2D/Control/SubViewportContainer/SubViewport/Fond/Map.texture = itex
 
 func _on_btn_sauvegarder_pressed() -> void:
 	planetGenerator.save_planet()
@@ -75,11 +86,11 @@ func _on_btn_suivant_pressed() -> void:
 	if map_index >= maps.size():
 		map_index = 0
 	
-	$Node2D/Node2D/Control/SubViewportContainer/SubViewport/Fond/Map.texture = ImageTexture.create_from_image(maps[map_index])
+	$Node2D/Control/SubViewportContainer/SubViewport/Fond/Map.texture = ImageTexture.create_from_image(maps[map_index])
 
 func _on_btn_precedant_pressed() -> void:
 	map_index -= 1
 	if map_index < 0:
 		map_index = maps.size() - 1
 	
-	$Node2D/Node2D/Control/SubViewportContainer/SubViewport/Fond/Map.texture = ImageTexture.create_from_image(maps[map_index])
+	$Node2D/Control/SubViewportContainer/SubViewport/Fond/Map.texture = ImageTexture.create_from_image(maps[map_index])
