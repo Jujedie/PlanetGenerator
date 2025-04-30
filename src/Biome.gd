0b5dd752cde1b78c2c939afbaa834eb1f66eaf0e
jujedie <oyer.ludovic@gmail.com>
2025-04-30 14:49:15 +0200

    Add Biome class with properties and methods for biome management

---
+++ src/Biome.gd
@@ -0,0 +1,53 @@
+extends RefCounted
+
+class_name Biome
+
+var nom: String
+var couleur: Color
+
+var interval_temp		  : Array[int]
+var interval_precipitation: Array[float]
+var interval_elevation    : Array[int]
+var water_need			  : bool
+
+# L'altitude minimale et maximale du biome dépend de l'élévation de la mer
+func _init(nom_param: String, couleur_param: Color, interval_temp_param: Array[int], interval_precipitation_param: Array[float], interval_elevation_param: Array[int], water_need_param: bool):
+	self.nom     = nom_param
+	self.couleur = couleur_param
+
+	self.interval_temp          = interval_temp_param
+	self.interval_precipitation = interval_precipitation_param
+	self.interval_elevation     = interval_elevation_param
+	self.water_need             = water_need_param
+
+func get_elevation_minimal() -> int:
+	return self.interval_elevation[0]
+func get_elevation_maximal() -> int:
+	return self.interval_elevation[1]
+func get_interval_temp() -> Array[int]:
+	return self.interval_temp
+func get_interval_precipitation() -> Array[float]:
+	return self.interval_precipitation
+func get_water_need() -> bool:
+	return self.water_need
+func get_nom() -> String:
+	return self.nom
+func get_couleur() -> Color:
+	return self.couleur
+
+func set_elevation_minimal(elevation_min: int):
+	self.interval_elevation[0] = elevation_min
+func set_elevation_maximal(elevation_max: int):
+	self.interval_elevation[1] = elevation_max
+func set_interval_temp(interval_temp: Array[int]):
+	self.interval_temp = interval_temp
+func set_interval_precipitation(interval_precipitation: Array[float]):
+	self.interval_precipitation = interval_precipitation
+func set_water_need(water_need: bool):
+	self.water_need = water_need
+func set_nom(nom: String):
+	self.nom = nom
+func set_couleur(couleur: Color):
+	self.couleur = couleur
+func set_interval_elevation(interval_elevation: Array[int]):
+	self.interval_elevation = interval_elevation
