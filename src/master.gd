b7a4b1f789536607c0c088ab133e3f938965054f
Jujedie <77742586+Jujedie@users.noreply.github.com>
2025-08-03 11:14:49 +0200

    Ajout de la prise en charge de l'internationalisation avec des traductions pour les éléments de l'interface utilisateur et mise à jour des étiquettes.

--- src/master.gd
+++ src/master.gd
@@ -1,50 +1,35 @@
 extends Node2D
 
-var planetGenerator  : PlanetGenerator
-var maps             : Array[String]
-var map_index        : int = 0
+var planetGenerator : PlanetGenerator
+var maps			: Array[String]
+var map_index		: int = 0
+var langue			: String = "fr"
 
 func _ready() -> void:
-	var sldRayonPlanetaire = $Node2D/Control/sldRayonPlanetaire
-	var label = $Node2D/Control/sldRayonPlanetaire/Node2D/Label
-	label.text = "Rayon Planétaire : "+str(sldRayonPlanetaire.value)
-	
-	var sldTempMoy = $Node2D/Control/sldTempMoy
-	label = $Node2D/Control/sldTempMoy/Node2D/Label
-	label.text = "Température Moyenne : "+str(sldTempMoy.value)
-	
-	var sldHautEau = $Node2D/Control/sldHautEau
-	label = $Node2D/Control/sldHautEau/Node2D/Label
-	label.text = "Elevation des mers : "+str(sldHautEau.value)
-	
-	var sldPrecipitationMoy = $Node2D/Control/sldPrecipitationMoy
-	label = $Node2D/Control/sldPrecipitationMoy/Node2D/Label
-	label.text = "Précipitation Moyenne : "+str(sldPrecipitationMoy.value)+" | 0 <-> 1\n"
+	if OS.get_locale_language() != "fr":
+		langue = "en"
 
-	var sldElevation = $Node2D/Control/sldElevation
-	label = $Node2D/Control/sldElevation/Node2D/Label
-	label.text = "Elevation bonus : "+str(sldElevation.value)
-
-	var sldThread = $Node2D/Control/sldThread
-	label = $Node2D/Control/sldThread/Node2D/Label
-	label.text = "Nombre de thread : "+str(sldThread.value)
+	TranslationServer.set_locale(langue)
+	
+	# Initialisation des paramètres de la planète
+	maj_labels()
 
 func _on_sld_rayon_planetaire_value_changed(value: float) -> void:
 	var sld = $Node2D/Control/sldRayonPlanetaire
 	var label = $Node2D/Control/sldRayonPlanetaire/Node2D/Label
-	label.text = "Rayon Planétaire : "+str(sld.value)
+	label.text = tr("RAYON_PLANET").format({"val": str(sld.value)})
 
 
 func _on_sld_temp_moy_value_changed(value: float) -> void:
 	var sld = $Node2D/Control/sldTempMoy
 	var label = $Node2D/Control/sldTempMoy/Node2D/Label
-	label.text = "Température Moyenne : "+str(sld.value)
+	label.text = tr("AVG_TEMP").format({"val": str(sld.value)})
 
 
 func _on_sld_haut_eau_value_changed(value: float) -> void:
 	var sld = $Node2D/Control/sldHautEau
 	var label = $Node2D/Control/sldHautEau/Node2D/Label
-	label.text = "Elevation des mers : "+str(sld.value)
+	label.text = tr("WATER_ELEVATION").format({"val": str(sld.value)})
 
 
 func _on_sld_precipitation_moy_value_changed(value: float) -> void:
@@ -53,7 +38,7 @@ func _on_sld_precipitation_moy_value_changed(value: float) -> void:
 	var value_str = str(sld.value)
 	if len(value_str) != 4:
 		value_str = value_str + "0"
-	label.text = "Précipitation Moyenne : "+value_str+" | 0 <-> 1"
+	label.text = tr("AVG_PRECIPITATION").format({"val": value_str})
 
 
 func _on_sld_percent_eau_value_changed(value: float) -> void:
@@ -62,19 +47,24 @@ func _on_sld_percent_eau_value_changed(value: float) -> void:
 	var value_str = str(sld.value)
 	if len(value_str) != 4:
 		value_str = value_str + "0"
-	label.text = "Pourcentage d'eau : "+value_str+" | 0 <-> 1"
+	label.text = tr("WATER_ELEVATION").format({"val": value_str})
 
 
 func _on_sld_elevation_value_changed(value: float) -> void:
 	var sld = $Node2D/Control/sldElevation
 	var label = $Node2D/Control/sldElevation/Node2D/Label
-	label.text = "Elevation bonus : "+str(sld.value)
+	label.text = tr("BONUS_ELEVATION").format({"val": str(sld.value)})
 
 func _on_sld_thread_value_changed(value: float) -> void:	
 	var sld = $Node2D/Control/sldThread
 	var label = $Node2D/Control/sldThread/Node2D/Label
-	label.text = "Nombre de thread : "+str(sld.value)
+	label.text = tr("THREAD_NUMBER").format({"val": str(sld.value)})
 
+
+func _on_sld_nb_cases_regions_value_changed(value: float) -> void:
+	var sld = $Node2D/Control/sldNbCasesRegions
+	var label = $Node2D/Control/sldNbCasesRegions/Node2D/Label
+	label.text = tr("NB_CASE_REGION").format({"val": str(sld.value)})
 
 func _on_btn_comfirme_pressed() -> void:
 	var nom = $Node2D/Control/planeteName/LineEdit
@@ -195,3 +185,51 @@ func _on_btn_precedant_pressed() -> void:
 
 func _on_btn_quitter_pressed() -> void:
 	get_tree().quit()
+
+func _on_btn_french_pressed() -> void:
+	if langue == "fr":
+		print("La langue est déjà en français.")
+		return
+	langue = "fr"
+	TranslationServer.set_locale(langue)
+	print("Langue changée en français.")
+	maj_labels()
+
+func _on_btn_english_pressed() -> void:
+	if langue == "en":
+		print("The language is already set to English.")
+		return
+	
+	langue = "en"
+	TranslationServer.set_locale(langue)
+	print("Language changed to English.")
+	maj_labels()
+
+func maj_labels() -> void:
+	var sldRayonPlanetaire = $Node2D/Control/sldRayonPlanetaire
+	var label = $Node2D/Control/sldRayonPlanetaire/Node2D/Label
+	label.text = tr("RAYON_PLANET").format({"val": str(sldRayonPlanetaire.value)})
+
+	var sldTempMoy = $Node2D/Control/sldTempMoy
+	label = $Node2D/Control/sldTempMoy/Node2D/Label
+	label.text = tr("AVG_TEMP").format({"val": str(sldTempMoy.value)})
+
+	var sldHautEau = $Node2D/Control/sldHautEau
+	label = $Node2D/Control/sldHautEau/Node2D/Label
+	label.text = tr("WATER_ELEVATION").format({"val": str(sldHautEau.value)})
+
+	var sldPrecipitationMoy = $Node2D/Control/sldPrecipitationMoy
+	label = $Node2D/Control/sldPrecipitationMoy/Node2D/Label
+	label.text = tr("AVG_PRECIPITATION").format({"val": str(sldPrecipitationMoy.value)})
+
+	var sldElevation = $Node2D/Control/sldElevation
+	label = $Node2D/Control/sldElevation/Node2D/Label
+	label.text = tr("BONUS_ELEVATION").format({"val": str(sldElevation.value)})
+
+	var sldThread = $Node2D/Control/sldThread
+	label = $Node2D/Control/sldThread/Node2D/Label
+	label.text = tr("THREAD_NUMBER").format({"val": str(sldThread.value)})
+
+	var sldNbCasesRegions = $Node2D/Control/sldNbCasesRegions
+	label = $Node2D/Control/sldNbCasesRegions/Node2D/Label
+	label.text = tr("NB_CASE_REGION").format({"val": str(sldNbCasesRegions.value)})
