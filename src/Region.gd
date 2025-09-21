06640acd59d246ccf2071d87424dc93ec558ec8b
Jujedie <77742586+Jujedie@users.noreply.github.com>
2025-09-21 18:16:12 +0200

    Amélioration de la gestion des couleurs dans la génération de régions : utilisation d'un mutex pour un accès sécurisé et réinitialisation du compteur de couleurs à chaque génération.

--- src/Region.gd
+++ src/Region.gd
@@ -2,38 +2,8 @@ extends RefCounted
 
 class_name Region
 
-static var colors : Array[Color] = [
-	Color.hex(0x672323FF),
-	Color.hex(0x4f1919FF),
-	Color.hex(0x341111FF),
-	Color.hex(0x670f0fFF),
-	Color.hex(0x4f0c0cFF),
-	Color.hex(0x3b0909FF),
-	Color.hex(0x793d3dFF),
-	Color.hex(0x5d2f2fFF),
-	Color.hex(0x412121FF),
-	Color.hex(0x442075FF),
-	Color.hex(0x34175aFF),
-	Color.hex(0x291345FF),
-	Color.hex(0x503475FF),
-	Color.hex(0x422c60FF),
-	Color.hex(0x2d1f41FF),
-	Color.hex(0x242c5aFF),
-	Color.hex(0x1e2448FF),
-	Color.hex(0x151934FF),
-	Color.hex(0x1a3d53FF),
-	Color.hex(0x153141FF),
-	Color.hex(0x112834FF),
-	Color.hex(0x134f39FF),
-	Color.hex(0x103e2cFF),
-	Color.hex(0x0d3727FF),
-	Color.hex(0x6b5f16FF),
-	Color.hex(0x6b5f16FF),
-	Color.hex(0x4c430fFF),
-	Color.hex(0x913926FF),
-	Color.hex(0x833424FF),
-	Color.hex(0x753122FF)
-]
+static var nextColor = [0,0,0,255]
+static var colorMutex = Mutex.new()
 
 var color : Color
 var ensVoisins : Array[Region]
@@ -44,7 +14,26 @@ var isComplete : bool
 
 func _init(nbCaseLeft_param: int) -> void:
 	self.nbCaseLeft = nbCaseLeft_param
-	self.color = color
+	
+	# Protéger l'accès concurrent à nextColor avec un mutex
+	colorMutex.lock()
+	
+	self.color = Color(nextColor[0] / 255.0, nextColor[1] / 255.0, nextColor[2] / 255.0, nextColor[3] / 255.0)
+
+	# Utiliser un pas plus grand pour créer des couleurs plus distinctes
+	var step = 17  # 255/17 ≈ 15 niveaux par canal, donnant beaucoup plus de couleurs distinctes
+	nextColor[0] += step
+	if nextColor[0] > 255:
+		nextColor[0] = nextColor[0] % 256
+		nextColor[1] += step
+	if nextColor[1] > 255:
+		nextColor[1] = nextColor[1] % 256
+		nextColor[2] += step
+	if nextColor[2] > 255:
+		nextColor[2] = nextColor[2] % 256
+	
+	colorMutex.unlock()
+
 	self.ensVoisins = []
 	self.cases = []
 	self.isComplete = false
@@ -69,20 +58,6 @@ func addVoisin(voisin: Region) -> void:
 	if not self.ensVoisins.has(voisin):
 		self.ensVoisins.append(voisin)
 
-
-func majColor() -> void:
-	var forbidden_colors = []
-	for i in range(0, self.ensVoisins.size()):
-		if self.ensVoisins[i].color != Color(0, 0, 0):
-			forbidden_colors.append(self.ensVoisins[i].color)
-	
-	var available_colors = []
-	for i in range(0, colors.size()):
-		if not forbidden_colors.has(colors[i]):
-			available_colors.append(colors[i])
-
-	self.color = colors[randi_range(0, available_colors.size() - 1)]
-
 func setColorCases(img: Image) -> void:
 	for case in self.cases:
 		var x = case[0]
@@ -90,19 +65,6 @@ func setColorCases(img: Image) -> void:
 		if img.get_pixel(x, y) != self.color:
 			img.set_pixel(x, y, self.color)
 
-func majNeighbors(ensCases: Dictionary) -> void:
-	for case in self.cases:
-		var x = case[0]
-		var y = case[1]
-		for dx in [-1, 0, 1]:
-			for dy in [-1, 0, 1]:
-				if ensCases.has(x + dx) and ensCases[x + dx].has(y + dy):
-					var voisin = ensCases[x + dx][y + dy]
-					if voisin != null and voisin != self:
-						self.addVoisin(voisin)
-						voisin.addVoisin(self)
-
-	majColor()
 
 func is_complete() -> bool:
 	return self.nbCaseLeft == 0
@@ -128,3 +90,8 @@ func is_potential_complete(ensCases: Dictionary) -> bool:
 		
 		self.isComplete = true
 		return true
+
+static func reset_color_counter() -> void:
+	colorMutex.lock()
+	nextColor = [0, 0, 0, 255]
+	colorMutex.unlock()
